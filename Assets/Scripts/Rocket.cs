fc101cb1843d36482357dcd16b368470d558535a
José Manuel <josemac95@gmail.com>
2019-08-06 13:31:10 +0200

    42. Basic input binding

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		ProcessInput();
+	}
+
+	// Procesa la entrada del usuario
+	private void ProcessInput()
+	{
+		// Movimiento
+		if (Input.GetKey(KeyCode.Space))
+		{
+			print("Thrusting");
+		}
+		// Rotación
+		if (Input.GetKey(KeyCode.A))
+		{
+			print("Rotating left");
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			print("Rotating right");
+		}
+	}
+}
