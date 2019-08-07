8335df50a214ea18be4dba69ef92514499ff6a52
José Manuel <josemac95@gmail.com>
2019-08-07 17:11:12 +0200

    50. Tagging game objects as friendly

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -7,38 +7,100 @@ public class Rocket : MonoBehaviour
 	// Componente RigidBody del cohete
 	Rigidbody rigidBody;
 
-	// Intensidad de la fuerza vertical (local)
-	[SerializeField]
-	float f = 10f;
+	// Componente de la fuente de audio
+	AudioSource audioSource;
+
+	// Intensidad de la fuerza vertical (coordenadas locales)
+	// El valor asignado será el por defecto, pero prevalece el
+	// que haya en el inspector
+	[SerializeField] float mainThrust = 1000f;
+
+	// Intensidad de la fuerza de rotación en el eje forward (local)
+	// El valor asignado será el por defecto, pero prevalece el
+	// que haya en el inspector
+	[SerializeField] float rcsThrust = 250f;
 
 	void Start()
 	{
 		// Versión gameObject.* hace lo mismo, pero es diferente
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	void Update()
 	{
-		ProcessInput();
+		Audio();
+		Thrust();
+		Rotate();
 	}
 
-	// Procesa la entrada del usuario
-	private void ProcessInput()
+	// Si choca con un objeto
+	void OnCollisionEnter(Collision collision)
+	{
+		switch (collision.gameObject.tag)
+		{
+			case "Friendly":
+				// No pasa nada
+				break;
+			default:
+				// Mueres
+				break;
+		}
+	}
+
+	private void Audio()
+	{
+		// Control de sonido
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			// Reproduce el sonido del cohete
+			audioSource.Play();
+		}
+		else if (Input.GetKeyUp(KeyCode.Space))
+		{
+			// Para el sonido del cohete
+			audioSource.Stop();
+		}
+	}
+
+	// Propulsión del cohete
+	private void Thrust()
 	{
 		// Movimiento
 		if (Input.GetKey(KeyCode.Space))
 		{
-			// Vector up como fuerza relativa
-			rigidBody.AddRelativeForce(f * Vector3.up);
+			// Fuerza relativa (coordenadas locales)
+			// a · t como velocidad
+			// Cantidad de propulsión para este frame
+			// Debe superar a la gravedad para despegar
+			rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
 		}
-		// Rotación
+	}
+
+	// Rota el cohete
+	private void Rotate()
+	{
+		// Control manual de la rotación
+		rigidBody.freezeRotation = true;
+
+		// Cantidad de rotación para este frame
+		// r · t es la velocidad de rotación
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+		// Rotación izquierda
 		if (Input.GetKey(KeyCode.A))
 		{
-			print("Rotating left");
+			// Coordenadas locales
+			transform.Rotate(Vector3.forward * rotationThisFrame);
 		}
+		// Rotación derecha
 		else if (Input.GetKey(KeyCode.D))
 		{
-			print("Rotating right");
+			// Coordenadas locales
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
 		}
+
+		// Restablece el control de físicas automático
+		rigidBody.freezeRotation = false;
 	}
 }
