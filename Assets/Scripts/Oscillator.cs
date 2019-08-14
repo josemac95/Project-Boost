97b6125ca3692ea944aa37bfe38a8c0011acd9f3
José Manuel <josemac95@gmail.com>
2019-08-14 11:45:16 +0200

    60. Moving platform pattern

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Esto hace que solamente se puede añadir un script de este tipo a un objeto
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+	// Posición original
+	Vector3 startingPosition;
+
+	// Vector de movimiento
+	[SerializeField] Vector3 movementVector;
+
+	// Hace que sea un slider [0, 1]
+	[Range(0, 1)]
+	// Factor de movimiento
+	// 0 posición original, 1 posición final
+	[SerializeField] float movementFactor;
+
+	void Start()
+	{
+		// Posicion inicial
+		startingPosition = transform.position;
+	}
+
+	void Update()
+	{
+		// Desplazamiento
+		Vector3 offset = movementFactor * movementVector;
+		// Mueve el objeto (manual)
+		transform.position = startingPosition + offset;
+	}
+}
